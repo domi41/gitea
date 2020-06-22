1ffe67aadcd225334cda717c159bf7722f608ee3
domi41 <dominique.merle@gmail.com>
2020-06-22 14:32:19 +0200

    feat: add the issue title to the list of issues of a poll
    Also naive and inefficient, but if we paginate, we'll be ok.
    And if not, we can probably improve this by batching the queries.

--- models/poll_result.go
+++ models/poll_result.go
@@ -5,9 +5,16 @@
 package models
 
 import (
-	//"code.gitea.io/gitea/modules/references"
 	"code.gitea.io/gitea/modules/timeutil"
+	"strconv"
 )
+
+//   ____                _ _     _       _       ____                 _ _
+//  / ___|__ _ _ __   __| (_) __| | __ _| |_ ___|  _ \ ___  ___ _   _| | |_
+// | |   / _` | '_ \ / _` | |/ _` |/ _` | __/ _ \ |_) / _ \/ __| | | | | __|
+// | |__| (_| | | | | (_| | | (_| | (_| | ||  __/  _ <  __/\__ \ |_| | | |_
+//  \____\__,_|_| |_|\__,_|_|\__,_|\__,_|\__\___|_| \_\___||___/\__,_|_|\__|
+//
 
 type PollCandidateResult struct {
 	Poll         *Poll
@@ -20,12 +27,38 @@ type PollCandidateResult struct {
 	CreatedUnix  timeutil.TimeStamp
 }
 
+func (result *PollCandidateResult) GetColorWord() (_ string) {
+	return result.Poll.GetGradeColorWord(result.MedianGrade)
+}
+
+func (result *PollCandidateResult) GetCandidateName() (_ string) { // FIXME
+	isssue, err := GetIssueByID(result.CandidateID + 1000)
+	if nil != err {
+		return "Candidate #" + strconv.FormatInt(result.CandidateID, 10)
+	}
+	return isssue.Title
+}
+
+//   ____                _ _     _       _       ____                 _ _
+//  / ___|__ _ _ __   __| (_) __| | __ _| |_ ___|  _ \ ___  ___ _   _| | |_ ___
+// | |   / _` | '_ \ / _` | |/ _` |/ _` | __/ _ \ |_) / _ \/ __| | | | | __/ __|
+// | |__| (_| | | | | (_| | | (_| | (_| | ||  __/  _ <  __/\__ \ |_| | | |_\__ \
+//  \____\__,_|_| |_|\__,_|_|\__,_|\__,_|\__\___|_| \_\___||___/\__,_|_|\__|___/
+//
+
 // PollCandidateResults implements sort.Interface based on the Score field.
 type PollCandidateResults []*PollCandidateResult
 
 func (a PollCandidateResults) Len() int           { return len(a) }
 func (a PollCandidateResults) Less(i, j int) bool { return a[i].Score < a[j].Score }
 func (a PollCandidateResults) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+
+//  ____                 _ _
+// |  _ \ ___  ___ _   _| | |_
+// | |_) / _ \/ __| | | | | __|
+// |  _ <  __/\__ \ |_| | | |_
+// |_| \_\___||___/\__,_|_|\__|
+//
 
 type PollResult struct {
 	Poll        *Poll
@@ -35,14 +68,11 @@ type PollResult struct {
 }
 
 func (result *PollResult) GetCandidate(candidateID int64) (_ *PollCandidateResult) {
+	// A `for` loop is pretty inefficient, index this somehow?
 	for _, candidate := range result.Candidates {
 		if candidate.CandidateID == candidateID {
 			return candidate
 		}
 	}
 	return nil
-}
-
-func (result *PollCandidateResult) GetColorWord() (_ string) {
-	return result.Poll.GetGradeColorWord(result.MedianGrade)
 }
